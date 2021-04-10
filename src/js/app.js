fe4526f80ec671acda1dc110d30aabc6be5f4cd9
Вячеслав Илькевич <ivada@MacBook-Pro-Vaceslav.local>
2021-04-10 13:28:48 +0300

    Done

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,22 @@
-export default class ErrorRepository{
- constructor(){
-     this.map = new Map([
-         [400, 'bad request'],
-         [401, 'unauthorisation'],
-         [402, 'payment required'],
-         [403, 'forbidden'],
-         [404, 'not found'],
-         [405, 'method not allowed'],
-         [406, 'not acceptable'],
-         [407, 'proxy authentication required'],
-     ]);
- };
+export default class ErrorRepository {
+  constructor() {
+    this.map = new Map([
+      [400, 'bad request'],
+      [401, 'unauthorisation'],
+      [402, 'payment required'],
+      [403, 'forbidden'],
+      [404, 'not found'],
+      [405, 'method not allowed'],
+      [406, 'not acceptable'],
+      [407, 'proxy authentication required'],
+    ]);
+  }
 
- translate(code){
-     if(this.map.has(code) === false) {
-        throw new Error('Unknown error');
-     } else{
-         return this.map.get(code);
-     }
- };
-};
+  translate(code) {
+    if (this.map.get(code)){
+        return this.map.get(code);
+    } 
+    return 'Unknown error'
+    }
+  
+}
